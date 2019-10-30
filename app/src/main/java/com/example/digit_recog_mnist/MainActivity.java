e280cca0bae15a48c5d82a97ccc90b94444e7662
Arnab Chanda <arnabchanda964@gmail.com>
2019-10-31 02:32:26 +0530

    Pre processing of image done.

--- app/src/main/java/com/example/digit_recog_mnist/MainActivity.java
+++ app/src/main/java/com/example/digit_recog_mnist/MainActivity.java
@@ -62,8 +62,8 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
     @Override
     public void onCameraViewStarted(int width, int height) {
         mat1 = new Mat(width,height, CvType.CV_8UC4);
-        mat2 = new Mat(width,height, CvType.CV_8UC4);
-        mat3 = new Mat(width,height, CvType.CV_8UC4);
+        mat2 = new Mat();
+        mat3 = new Mat();
         imageToanalyse = new Mat(28,28,CvType.CV_8UC4);
     }
 
@@ -77,24 +77,49 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
 
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        mat1 = inputFrame.rgba();
-        Core.transpose(mat1,mat2);
-        Core.flip(mat2,mat1,1);
 
-        int w = mat1.width();
-        int h = mat1.height();
-
-        Imgproc.rectangle(mat1, new Point(w * 1 / 3, h * 1 / 3), new Point(
-                w * 2 / 3, h * 2 /  3 ), new Scalar( 255, 0, 0 ), 5
-        );
-        Rect roi  = new Rect(w*1/3,h*1/3,(w*2/3 - w*1/3),(h*2/3-h*1/3));
-        Mat temp = mat1.submat(roi);
+        mat1=inputFrame.rgba();
+        Core.transpose(mat1,mat1);
+        Core.flip(mat1,mat1,1);
 
 
-        Imgproc.resize(temp,imageToanalyse,imageToanalyse.size(),0,0,0);
+        int top = mat1.rows()/2 - 140;
+        int left = mat1.cols() / 2 - 140;
+        int height = 140*2;
+        int width = 140*2;
 
+        ///prepocess frame
+        Mat gray = inputFrame.gray();
+        //draw cropped region
+        Imgproc.rectangle(mat1, new Point(mat1.cols()/2 - 150, mat1.rows() / 2 - 150), new Point(mat1.cols() / 2 + 150, mat1.rows() / 2 + 150), new Scalar(255,255,255),5);
+        //crop frame
+        Mat graytemp = gray.submat(top, top + height, left, left + width);
+        //blur the cropped frame to remove noise
+        Imgproc.GaussianBlur(graytemp, graytemp, new org.opencv.core.Size(7,7),2 , 2);
+        //convert gray frame to binary using apadative thresold
+        Imgproc.adaptiveThreshold(graytemp, mat2, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 5, 5);
+        Mat element1 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new org.opencv.core.Size(9,9));
+        //dilate the frame
+        Imgproc.dilate(mat2, mat2, element1);
+
+        Mat element = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new org.opencv.core.Size(3,3));
+        //erode the frame
+        Imgproc.erode(mat2, mat2, element);
+
+        Imgproc.resize(mat2, imageToanalyse, new org.opencv.core.Size(28,28));///CNN input
+        Core.transpose(imageToanalyse,imageToanalyse);
+        Core.flip(imageToanalyse,imageToanalyse,1);
+
+        ///use this to classify camera feed
+
+        //classifier.classifyMat(CNN_input);
+        //Imgproc.putText(mRgba, "Digit: "+classifier.getdigit()+ " Prob: "+classifier.getProb(), new Point(top, left), 3, 3, new Scalar(255, 0, 0, 255), 2);
+
+        
         img = Bitmap.createBitmap(imageToanalyse.cols(), imageToanalyse.rows(), Bitmap.Config.ARGB_8888);
         Utils.matToBitmap(imageToanalyse, img);
+
+        graytemp.release();
 
         runOnUiThread(new Runnable() {
             @Override
@@ -103,7 +128,6 @@ public class MainActivity extends AppCompatActivity implements CameraBridgeViewB
                     myImageView.setImageBitmap(img);
             }
         });
-
 
         return mat1;
     }
